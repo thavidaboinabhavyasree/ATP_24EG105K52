fa19a39cc06225d06ef73859d1e3290975cf4e6d
thavidaboinabhavyasree <thavidaboinabhavyasree@gmail.com>
2026-03-02 20:24:13 +0530

    Update 1.3.js

--- Week 1 assignments/1.3.js
+++ Week 1 assignments/1.3.js
@@ -1,9 +1,10 @@
 let a=33;
 let b=99;
 let c=66;
-if(a>b,a>c)
+if(a>b&&a>c)
     console.log("Greatest number is:",a)
-else if(b>a,b>c)
+else if(b>a&&b>c)
   console.log("Greatest number is:",b)
 else
+
     console.log("Greatest number is",c)
